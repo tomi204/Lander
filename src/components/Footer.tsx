aa32e89fd1933e9c1d36cc4ab05c63491374c002
dexalien <caroldelaquintana@gmail.com>
2024-08-28 07:42:57 -0300

    Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Logo from "@/shared/Logo";
-import SocialsList1 from "@/shared/SocialsList1";
+//import SocialsList1 from "@/shared/SocialsList1";
 import { CustomLink } from "@/data/types";
 import React from "react";
 import FooterNav from "./FooterNav";
@@ -91,11 +91,11 @@ const Footer: React.FC = () => {
         <div className="container grid grid-cols-2 gap-y-10 gap-x-5 sm:gap-x-8 md:grid-cols-4 lg:grid-cols-5 lg:gap-x-10 ">
           <div className="grid grid-cols-4 gap-5 col-span-2 md:col-span-4 lg:md:col-span-1 lg:flex lg:flex-col">
             <div className="col-span-2 md:col-span-1 sm:hidden">
-              <Logo />
+              <Logo /> <p>By SmartBlocks</p>
             </div>
-            <div className="col-span-2 flex items-center md:col-span-3">
+            {/* <div className="col-span-2 flex items-center md:col-span-3">
               <SocialsList1 className="flex items-center space-x-3 lg:space-x-0 lg:flex-col lg:space-y-2.5 lg:items-start" />
-            </div>
+            </div> */}
           </div>
           {/* {widgetMenus.map(renderWidgetMenuItem)} */}
         </div>
