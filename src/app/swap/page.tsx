d78dc655f6baf5c4a3e23e4a7ddb5c05c4431026
Tomas Oliver <tomasoliver@Tomass-MacBook-Pro.local>
2024-09-14 18:22:49 -0300

    styles in swap page

--- src/app/swap/page.tsx
+++ src/app/swap/page.tsx
@@ -1,5 +1,16 @@
 'use client';
+import { Dialog, DialogHeader } from '@/components/ui/dialog';
 import { Swap } from '../../components/Swap/index';
+import { DialogDescription, DialogTitle } from '@radix-ui/react-dialog';
+
 export default function SwapPage() {
-  return <Swap />;
+  return (
+    <div className="min-h-screen bg-white p-8">
+      <h1 className="text-4xl font-bold text-yellow-500 mb-8 text-center">
+        Swap / Bridge
+      </h1>
+
+      <Swap />
+    </div>
+  );
 }
