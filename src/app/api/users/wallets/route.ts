8bde4d4c3045926171639789c2467897fb77e4b4
humancodex <juansebcarv@outlook.com>
2024-10-12 19:38:07 -0300

    Refactor fetchUsers function to select all fields from the 'wallets' table

--- src/app/api/users/wallets/route.ts
+++ src/app/api/users/wallets/route.ts
@@ -1,8 +1,8 @@
 import { NextResponse } from 'next/server';
 import supabase from '@/utils/supabase/client';
 
-const fetchUsers = async () => {
-  const { data, error } = await supabase.from('users').select('id, wallet_evm');
+export const fetchUsers = async () => {
+  const { data, error } = await supabase.from('wallets').select('*');
 
   if (error) {
     console.error('Error fetching users:', NextResponse.json(error));
